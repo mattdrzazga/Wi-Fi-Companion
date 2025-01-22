edcf05770942e9a132826ed70ecc37dacdf5fbcf
Mateusz Drzazga <matt.drzazga@gmail.com>
2025-01-22 18:17:47 +0100

    Allow device admin removal

--- app/src/main/java/com/mattdrzazga/wificompanion/MainActivity.kt
+++ app/src/main/java/com/mattdrzazga/wificompanion/MainActivity.kt
@@ -17,10 +17,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // Clear device admin if needed
         val administrator = Administrator(this)
         val shouldClearDeviceAdmin = intent.extras?.containsKey(CLEAR_DEVICE_ADMIN) ?: false
         if (shouldClearDeviceAdmin && administrator.isDeviceAdmin()) {
             administrator.removeDeviceOwnerApp()
+            log("Removed device admin")
             finish()
             return
         }
@@ -29,5 +31,10 @@ class MainActivity : AppCompatActivity() {
     companion object {
 
         const val CLEAR_DEVICE_ADMIN = "clear_device_admin"
+
+        private const val CMD_CLEAR_ADMIN = "adb shell am start -n com.mattdrzazga.wificompanion/.MainActivity --ez clear_device_admin true"
+
+        private const val ADB_CLEAR_ADMIN = "adb shell dpm remove-active-admin com.mattdrzazga.wificompanion/.BindAdminReceiver"
+        private const val ADB_SET_ADMIN = "adb shell dpm set-device-owner com.mattdrzazga.wificompanion/.BindAdminReceiver"
     }
 }
