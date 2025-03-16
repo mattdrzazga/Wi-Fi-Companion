b4e35448e1c7eaee945e5f62ddb68b0a692ea9dc
Mateusz Drzazga <matt.drzazga@gmail.com>
2025-03-16 17:06:39 +0100

    Add methods to check for post notification permission

--- app/src/main/java/com/mattdrzazga/wificompanion/Permission.kt
+++ app/src/main/java/com/mattdrzazga/wificompanion/Permission.kt
@@ -2,12 +2,14 @@ package com.mattdrzazga.wificompanion
 
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.Manifest.permission.ACCESS_WIFI_STATE
+import android.Manifest.permission.POST_NOTIFICATIONS
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import androidx.core.app.ActivityCompat
 
-const val PERMISSION_REQUEST_CODE = 10001
+const val LOCATION_PERMISSION_REQUEST_CODE = 10000
+const val POST_NOTIFICATION_PERMISSION_REQUEST_CODE = 10001
 
 fun Context.hasNetworkPermission(): Boolean {
     val fineLocationGranted = ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED
@@ -15,6 +17,16 @@ fun Context.hasNetworkPermission(): Boolean {
     return fineLocationGranted && accessWifiStateGranted
 }
 
-fun Activity.requestNetworkPermission() {
-    this.requestPermissions(arrayOf(ACCESS_FINE_LOCATION, ACCESS_WIFI_STATE), PERMISSION_REQUEST_CODE)
+fun Activity.requestNetworkPermission() =
+    requestPermissions(
+        arrayOf(ACCESS_FINE_LOCATION, ACCESS_WIFI_STATE),
+        LOCATION_PERMISSION_REQUEST_CODE
+    )
+
+fun Context.hasPostNotificationPermission(): Boolean {
+    return ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PERMISSION_GRANTED
+}
+
+fun Activity.requestPostNotificationPermission() {
+    this.requestPermissions(arrayOf(POST_NOTIFICATIONS), POST_NOTIFICATION_PERMISSION_REQUEST_CODE)
 }
