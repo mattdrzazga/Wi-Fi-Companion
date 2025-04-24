6508c84c884d4cc230aa1ca0c6db2066cd72c4ac
Mateusz Drzazga <matt.drzazga@gmail.com>
2025-04-24 20:56:07 +0200

    Add warning log

--- app/src/main/java/com/mattdrzazga/wificompanion/Log.kt
+++ app/src/main/java/com/mattdrzazga/wificompanion/Log.kt
@@ -5,3 +5,7 @@ import android.util.Log
 fun Any.log(message: String, tag: String = this.javaClass.simpleName) {
     Log.v(tag, message)
 }
+
+fun Any.logw(message: String, tag: String = this.javaClass.simpleName) {
+    Log.w(tag, message)
+}
